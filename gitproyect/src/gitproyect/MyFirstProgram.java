b963cc09a4c1d0a2cfe29bff949c961bf973ff75
roberto corona <robertocorona74@hotmail.com>
2016-01-21 19:02:45 -0800

    h

--- gitproyect/src/gitproyect/MyFirstProgram.java
+++ gitproyect/src/gitproyect/MyFirstProgram.java
@@ -4,6 +4,6 @@ public class MyFirstProgram
 {
 	public static void main(String[] args) {
 		
-		System.out.println("MyFirstProgram.main() Hello World  2016-01!!");
+		System.out.println("MyFirstProgram.main() Hello World  2016-Jan!!");
 	}
 }
